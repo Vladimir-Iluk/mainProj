70c2b85bf5db308f49c0060278ca6799e82551b8
Vladimir-Iluk <nezalejniy@ukr.net>
2025-05-25 13:11:22 +0300

    first commit

---
+++ src/main/java/com/iluk/git/mainProj/MainProjApplication.java
@@ -0,0 +1,13 @@
+package com.iluk.git.mainProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iluk/git/mainProj/MainProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iluk.git.mainProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
