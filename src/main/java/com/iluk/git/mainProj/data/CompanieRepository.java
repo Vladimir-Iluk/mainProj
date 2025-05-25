b122b57b0d180a6d40b453e82c5d23e37469eace
exodiums <nezalejniy@ukr.net>
2025-05-25 15:03:54 +0300

    added crud repository for every table

---
+++ src/main/java/com/iluk/git/mainProj/data/CompanieRepository.java
@@ -0,0 +1,6 @@
+package com.iluk.git.mainProj.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanieRepository extends CrudRepository<Companie, Long> {
+}
---
+++ src/main/java/com/iluk/git/mainProj/data/EmployerRepository.java
@@ -0,0 +1,6 @@
+package com.iluk.git.mainProj.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployerRepository extends CrudRepository<Employer, Long> {
+}
